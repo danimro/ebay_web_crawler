3db150b62866ec55fcd30cd221047701fbbdc523
danimro <danimro@post.bgu.ac.il>
2020-11-05 20:22:17 +0200

    Scraping one ePID

--- crawler.py
+++ crawler.py
@@ -4,8 +4,8 @@ import requests
 
 link = "https://www.ebay.com/p/1104956473"
 
-req = Request(link, headers={'User-Agent' : 'Mozilla/5.0'})
+req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
 webpage = urlopen(req).read()
 with requests.Session() as c:
     soup = BeautifulSoup(webpage, 'html5lib')
-    print(soup)
+    print(soup.findAll("div", attrs={"class": "s-value"})[3].text)
