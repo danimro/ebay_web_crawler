931348aab9566b531941db6f787c0f1d61971e15
danimro <danimro@post.bgu.ac.il>
2020-11-05 19:30:04 +0200

    Initial commit

---
+++ crawler.py
@@ -0,0 +1,11 @@
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+import requests
+
+link = "https://www.ebay.com/p/1104956473"
+
+req = Request(link, headers={'User-Agent' : 'Mozilla/5.0'})
+webpage = urlopen(req).read()
+with requests.Session() as c:
+    soup = BeautifulSoup(webpage, 'html5lib')
+    print(soup)
